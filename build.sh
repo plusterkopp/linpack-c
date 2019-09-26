981c845f7bcac67813f28080d86dc8bd2ffe1612
Ralf H <ralf@pkmd.de>
2019-09-26 23:22:14 +0200

    alles besser

--- build.sh
+++ build.sh
@@ -1,45 +1,65 @@
 
-GCCArchitectures="nocona core2 nehalem corei7 westmere sandybridge corei7-avx ivybridge core-avx-i haswell core-avx2 broadwell skylake skylake-avx512 cannonlake icelake-client icelake-server cascadelake bonnell atom silvermont slm goldmont goldmont-plus tremont knl knm intel x86-64 eden-x2 nano nano-1000 nano-2000 nano-3000 nano-x2 eden-x4 nano-x4 k8 k8-sse3 opteron opteron-sse3 athlon64 athlon64-sse3 athlon-fx amdfam10 barcelona bdver1 bdver2 bdver3 bdver4 znver1 znver2 btver1 btver2 generic native"
-ClangArchitectures="nocona core2 penryn bonnell atom silvermont slm goldmont goldmont-plus tremont nehalem corei7 westmere sandybridge corei7-avx ivybridge core-avx-i haswell core-avx2 broadwell skylake skylake-avx512 skx cascadelake cannonlake icelake-client icelake-server knl knm k8 athlon64 athlon-fx opteron k8-sse3 athlon64-sse3 opteron-sse3 amdfam10 barcelona btver1 btver2 bdver1 bdver2 bdver3 bdver4 znver1 x86-64"
+if [ $(uname -m) == x86_64 ]; then
+	GCCArchitectures="nocona core2 nehalem corei7 westmere sandybridge corei7-avx ivybridge core-avx-i haswell core-avx2 broadwell skylake skylake-avx512 cannonlake icelake-client icelake-server cascadelake bonnell atom silvermont slm goldmont goldmont-plus tremont knl knm intel x86-64 eden-x2 nano nano-1000 nano-2000 nano-3000 nano-x2 eden-x4 nano-x4 k8 k8-sse3 opteron opteron-sse3 athlon64 athlon64-sse3 athlon-fx amdfam10 barcelona bdver1 bdver2 bdver3 bdver4 znver1 znver2 btver1 btver2 generic native"
+	ClangArchitectures="nocona core2 penryn bonnell atom silvermont slm goldmont goldmont-plus tremont nehalem corei7 westmere sandybridge corei7-avx ivybridge core-avx-i haswell core-avx2 broadwell skylake skylake-avx512 skx cascadelake cannonlake icelake-client icelake-server knl knm k8 athlon64 athlon-fx opteron k8-sse3 athlon64-sse3 opteron-sse3 amdfam10 barcelona btver1 btver2 bdver1 bdver2 bdver3 bdver4 znver1 x86-64"
+elif [ $(uname -m) == armv7l ]; then
+	GCCArchitectures="armv4 armv4t armv5t armv5te armv5tej armv6 armv6j armv6k armv6z armv6kz armv6zk armv6t2 armv6-m armv6s-m armv7 armv7-a armv7ve armv7-r armv7-m armv7e-m armv8-a armv8.1-a armv8.2-a armv8.3-a armv8.4-a armv8.5-a armv8-m.base armv8-m.main armv8-r iwmmxt iwmmxt2 native"
+	ClangArchitectures="nocona core2 penryn bonnell atom silvermont slm goldmont goldmont-plus tremont nehalem corei7 westmere sandybridge corei7-avx ivybridge core-avx-i haswell core-avx2 broadwell skylake skylake-avx512 skx cascadelake cannonlake icelake-client icelake-server knl knm k8 athlon64 athlon-fx opteron k8-sse3 athlon64-sse3 opteron-sse3 amdfam10 barcelona btver1 btver2 bdver1 bdver2 bdver3 bdver4 znver1 x86-64"
+else
+	echo unknown architecture
+	exit 1
+fi
 
 Name=linpack
 
 GCCFLAGS="-O3 -fexpensive-optimizations"
 CLANGFLAGS="-Ofast"
 
-GCC=gcc
-CLANG=clang-8
+GCC=/usr/local/bin/gcc
+CLANG=/usr/bin/clang
 
-for arch in $GCCArchitectures
+if [ "$1" == "-b" ]; then
+	for arch in $GCCArchitectures
+	do
+	#	echo building $arch
+		if [ $arch != generic ]; then
+			$GCC $GCCFLAGS -mtune=$arch -march=$arch -o ${Name}-$arch ${Name}.c &
+		else
+			$GCC $GCCFLAGS  -o ${Name}-$arch ${Name}.c &
+		fi
+	done
+	for arch in $ClangArchitectures
+	do
+	#	echo building $arch
+		if [ $arch != generic ]; then
+			$CLANG $CLANGFLAGS -mtune=$arch -march=$arch -o ${Name}C-$arch ${Name}.c &
+		else
+			$CLANG $CLANGFLAGS  -o ${Name}C-$arch ${Name}.c &
+		fi
+	done
+	wait	# Compile-Jobs abwarten
+fi
+
+Combined=$GCCArchitectures" "$ClangArchitectures
+IFS=' ' read -ra Split <<< "${Combined}"
+Sorted=($(printf '%s\n' "${Split[@]}" | sort -u))
+
+Machine=`uname -n`
+LogName=results-${Machine}.log
+
+echo > ${LogName}
+
+for arch in "${Sorted[@]}" 
 do
-#	echo building $arch
-	if [ $arch != generic ]; then
- 		$GCC $GCCFLAGS -mtune=$arch -march=$arch -o ${Name}-$arch ${Name}.c &
-	else
-		$GCC $GCCFLAGS  -o ${Name}-$arch ${Name}.c &
+	GCCFile=${Name}-$arch
+	if [ -x $GCCFile ]; then
+		echo running $GCCFile
+		./$GCCFile | tee -a ${LogName}
+	fi
+	ClangFile=${Name}C-$arch
+	if [ -x $ClangFile ]; then
+		echo running $ClangFile
+		./$ClangFile | tee -a ${LogName}
 	fi
 done
 
-for arch in $ClangArchitectures
-do
-#	echo building $arch
-	if [ $arch != generic ]; then
-		$CLANG $CLANGFLAGS -mtune=$arch -march=$arch -o ${Name}C-$arch ${Name}.c &
-	else
-		$CLANG $CLANGFLAGS  -o ${Name}C-$arch ${Name}.c &
-	fi
-done
-
-wait
-
-for arch in $GCCArchitectures
-do
-	echo running ${Name}-$arch
-	./${Name}-$arch | tee -a results.log
-done
-
-for arch in $ClangArchitectures
-do
-	echo running ${Name}C-$arch
-	./${Name}C-$arch | tee -a results.log
-done
